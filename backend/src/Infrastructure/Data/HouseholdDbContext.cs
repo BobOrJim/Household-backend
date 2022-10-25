f5629ed2496adcb3394167282cd1a9de09dc24fd
Jimmy Nordin <jimmy.nordin.1979@gmail.com>
2022-10-25 23:40:15 +0200

    back to in memory

--- backend/src/Infrastructure/Data/HouseholdDbContext.cs
+++ backend/src/Infrastructure/Data/HouseholdDbContext.cs
@@ -26,9 +26,9 @@ public class HouseholdDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer(azureConnectionString); //When using Azure Db version
+        //optionsBuilder.UseSqlServer(azureConnectionString); //When using Azure Db version
         // optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=HouseholdDb;Trusted_Connection=True;"); //When using local Db version
-        //optionsBuilder.UseInMemoryDatabase("inMemory"); //When using inMemory
+        optionsBuilder.UseInMemoryDatabase("inMemory"); //When using inMemory
     }
 
 
